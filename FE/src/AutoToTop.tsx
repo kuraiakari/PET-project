6fcd875e9822e07a5498d817c908592b8cf22c5d
tuan <thienbinh04102000@gmail.com>
2023-05-17 14:02:49 +0300

    [fix] auto back to top when change query

--- FE/src/AutoToTop.tsx
+++ FE/src/AutoToTop.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useSearchParams } from 'react-router-dom'
 
 export default function AutoScrollToTop() {
   const { pathname } = useLocation()
+  const [query] = useSearchParams()
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [pathname])
+  }, [pathname, query])
 
   return null
 }
